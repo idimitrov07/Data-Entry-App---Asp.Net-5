e563a252951a45a2da0336b1516f5e71ee088c22
Ivan <idimitrov07@gmail.com>
2014-03-30 10:37:14 +0300

    Added model, view for the input form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,9 +12,34 @@ namespace PartyInvites.Controllers
         // GET: /Home/ 
         public ViewResult Index()
         {
-            int hour = DateTime.Now.Hour;
-            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            Greeting();
             return View();
         }
-	}
+
+        //action method for the input form
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+
+
+        //method for getting the right greeting 
+        private void Greeting()
+        {
+            int hour = DateTime.Now.Hour;
+            if (hour < 12)
+            {
+                ViewBag.Greeting = "Good Morning";
+            }
+            else if (hour < 18)
+            {
+                ViewBag.Greeting = "Good Afternoon";
+            }
+            else
+            {
+                ViewBag.Greeting = "Good Evening";
+            }
+        }
+    }
 }
